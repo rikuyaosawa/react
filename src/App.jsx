4facc1a57db3700cff65695fae9935b42edfd777
Osawa Rikuya <rikuyaosawa@gmail.com>
2024-10-22 15:26:00 +0900

    react props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Button from "./Button/Button.jsx";
-import Card from "./Card.jsx";
+import Student from "./Student.jsx";
 
 function App() {
     return (
         <>
-            <Card />
-            <Button />
+            <Student name="SpongeBob" age={30} isStudent={true} />
+            <Student name="Patrick" age={42} isStudent={false} />
+            <Student name="Squidward" age={50} isStudent={false} />
+            <Student name="Sandy" age={27} isStudent={true} />
+            <Student />
         </>
     );
 }
