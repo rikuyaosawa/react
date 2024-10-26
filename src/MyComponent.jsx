db79f9a1045e9f5a707f618555f24b93dba3b61e
rikuyaosawa <rikuyaosawa@gmail.com>
2024-10-26 20:47:36 +0900

    updater function

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,20 +1,27 @@
 import { useState } from "react";
-import { Heading } from "@chakra-ui/react";
-import { Input } from "@chakra-ui/react";
-
+import { Button, Heading } from "@chakra-ui/react";
 function MyComponent() {
-    const [name, setName] = useState("Guest");
-    const handleNameChange = (event) => {
-        setName(event.target.value);
-    };
+    const [count, setCount] = useState(0);
+
+    function increment() {
+        setCount((c) => c + 1);
+    }
+    function reset() {
+        setCount((c) => c - c);
+    }
+    function decrement() {
+        setCount((c) => c - 1);
+    }
     return (
         <div>
-            <Heading>Name: {name}</Heading>
-            <Input
-                name={name}
-                onChange={handleNameChange}
-                placeholder="Type in name..."
-            />
+            <Heading>Count: {count}</Heading>
+            <Button onClick={increment} variant="outline">
+                Increment
+            </Button>
+            <Button onClick={reset} colorScheme="red">
+                Reset
+            </Button>
+            <Button onClick={decrement}>Decrement</Button>
         </div>
     );
 }
